5cb36e719296ff918e672c4ad4064af87028874c
Anthony Hill <hillan1152@gmail.com>
2019-12-05 09:02:07 -0500

    setting up CRUD and added Validate UserId

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server');
+
+server.listen(3535, () => {
+    console.log(`\n Server Running On http://localhost:3535`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+// IMPORT DATA
+const userDb = require('./userDb');
+
 router.post('/', (req, res) => {
   // do your magic!
 });
@@ -12,10 +15,33 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDb.get(req.query) 
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving data.'})
+    })
+
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id
+
+  userDb.getById(id)
+    .then(user => {
+      console.log('This is user', user)
+      if(user){
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({ message: "Specified id does not exist"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+  
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -34,6 +60,14 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id;
+
+  if(id !== req.user.id){
+    res.status(400).json({ message: "invalid user id" })
+  } else {
+    res.json({ user: req.user })
+    next()
+  }
 }
 
 function validateUser(req, res, next) {
