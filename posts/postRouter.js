ae401c6f2082733d00ae7202447d3b0c20cb3a54
Anthony Hill <hillan1152@gmail.com>
2019-12-06 00:00:35 -0500

    setting up postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,19 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
+// IMPORT DATA
+const userDb = require('../users/userDb');
+const postDb = require('./postDb');
+
 router.get('/', (req, res) => {
   // do your magic!
+  postDb.get(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving Posts.'})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
@@ -22,6 +33,20 @@ router.put('/:id', (req, res) => {
 
 function validatePostId(req, res, next) {
   // do your magic!
+  const id = req.params.id;
+
+  postDb.getById(id)
+    .then(post => {
+      if(!post){
+        res.status(400).json({ message: "Invalid post ID."})
+      } else {
+        req.post = post;
+        next()
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Post Information Untouchable! "})
+    })
 }
 
 module.exports = router;
