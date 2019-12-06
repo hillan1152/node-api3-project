30449bc755d45232838d9d5eb3a4117fe1f92a6d
Anthony Hill <hillan1152@gmail.com>
2019-12-05 19:25:36 -0500

    changed send

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use('/api/users', logger, userRouter);
 server.use('/api/posts', logger, postRouter);
 
 server.get('/', logger, (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`This is the node-api-3 project`);
 });
 
 //custom middleware
